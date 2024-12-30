277ae420e2c2b5400488705b4dbfcfe1f32da415
hyobiin9 <moone1234@kakao.com>
2024-12-30 17:14:39 +0900

    delete: clean landing page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,5 @@
 import { ModalWrapper } from "@/components/common/headless/Modal";
 
 export default function Home() {
-  return (
-    <div>
-      <ModalWrapper onClose={() => console.log("모달 닫기")}>
-        <ModalWrapper.Header>모달 제목</ModalWrapper.Header>
-        <ModalWrapper.Content>
-          <p>모달 내용이 여기에 들어갑니다.</p>
-        </ModalWrapper.Content>
-        <ModalWrapper.Footer isDisabled={false}>버튼 확인</ModalWrapper.Footer>
-      </ModalWrapper>
-    </div>
-  );
+  return <div></div>;
 }
