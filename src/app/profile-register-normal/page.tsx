20561cb3c3f7cdce8abad9dfcdd0205869be91e3
unknown <d653@naver.com>
2025-01-03 13:46:30 +0900

    Feat:#21 - profile register page structure

---
+++ src/app/profile-register-normal/page.tsx
@@ -0,0 +1,21 @@
+export default function ProfileRegisterNormal() {
+  return (
+    <div>
+      <div>
+        <h1>프로필 등록</h1>
+        <h3>추가 정보를 입력하여 회원가입을 완료해주세요.</h3>
+      </div>
+      <div>
+        <h2>프로필 이미지</h2>
+      </div>
+      <div>
+        <h2>이용 서비스</h2>
+        <p>*이용 서비스는 중복 선택 가능하며, 언제든 수정 가능해요!</p>
+      </div>
+      <div>
+        <h2>내가 사는 지역</h2>
+        <p>*내가 사는 지역은 언제든 수정 가능해요!</p>
+      </div>
+    </div>
+  );
+}
