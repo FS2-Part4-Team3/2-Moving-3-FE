c79facf0be3d8ffcc4e01e3b9be647b5cafd8ed6
hyobiin9 <moone1234@kakao.com>
2025-01-03 23:22:06 +0900

    style: box shadow style

--- src/components/cards/FindDriverCard.tsx
+++ src/components/cards/FindDriverCard.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 
 export default function FindDriverCard({ data }: FindDriverCardProps) {
   return (
-    <div className="w-[95.5rem] h-[23rem] rounded-[1.rem] border-0.5 border-line-100 py-[2rem] px-[2.4rem] shadow-lg">
+    <div
+      className="w-[95.5rem] h-[23rem] rounded-[1.6rem] border-0.5 border-line-100 py-[2rem] px-[2.4rem]"
+      style={{ boxShadow: "-2px -2px 10px rgba(220, 220, 220, 0.3)" }}
+    >
       <div className="flex gap-[1.2rem]">
         {data.serviceType.map((item: string, index) => (
           <MovingTypeChips
