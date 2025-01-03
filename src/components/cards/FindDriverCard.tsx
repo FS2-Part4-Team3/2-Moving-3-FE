2c5053bb060c54913277a1d5747b7875e6c3e90e
hyobiin9 <moone1234@kakao.com>
2025-01-03 22:57:47 +0900

    feat: find driver card initial ui

--- src/components/cards/FindDriverCard.tsx
+++ src/components/cards/FindDriverCard.tsx
@@ -1,5 +1,6 @@
 import type { FindDriverCardProps } from "@/interfaces/Card/FindDriverCardInterface";
 import MovingTypeChips from "../chips/MovingTypeChips";
+import Image from "next/image";
 
 export default function FindDriverCard({ data }: FindDriverCardProps) {
   return (
@@ -13,6 +14,21 @@ export default function FindDriverCard({ data }: FindDriverCardProps) {
             }
           />
         ))}
+      </div>
+      <p>{data.introduce}</p>
+      <div>
+        <Image src={data.image} alt="driver-profile" width={56} height={56} />
+        <div>
+          <div>
+            <p>{data.name}</p>
+            <p>{data.favoriteCount}</p>
+          </div>
+          <div>
+            <p>{data.score}</p>
+            <p>경력 {data.career}</p>
+            <p>{data.applyCount}건 확정</p>
+          </div>
+        </div>
       </div>
     </div>
   );
