a4b28c9d2c9b41375d920cf871aa178df0348651
hyobiin9 <moone1234@kakao.com>
2025-01-04 00:30:28 +0900

    feat: initial ui

---
+++ src/components/dropdown/RegionServiceDropdown.tsx
@@ -0,0 +1,17 @@
+export default function RegionServiceDropdown() {
+  return (
+    <div>
+      <div>
+        <p>필터</p>
+        <p>초기화</p>
+      </div>
+      <div />
+      <div>
+        <p>지역을 선택해주세요</p>
+      </div>
+      <div>
+        <p>어떤 서비스가 필요하세요?</p>
+      </div>
+    </div>
+  );
+}
