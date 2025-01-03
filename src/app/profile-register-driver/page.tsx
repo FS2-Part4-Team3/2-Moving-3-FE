e22d36fa6ff30204b44215ba2ba7b62432f317ed
unknown <d653@naver.com>
2025-01-03 15:57:46 +0900

    Feat:#24 - profile register page structure

---
+++ src/app/profile-register-driver/page.tsx
@@ -0,0 +1,29 @@
+export default function ProfileRegisterDriver() {
+  return (
+    <div>
+      <div>
+        <h1>기사님 프로필 등록</h1>
+        <h3>추가 정보를 입력하여 회원가입을 완료해주세요.</h3>
+      </div>
+      <div>
+        <div>
+          <div>
+            <h3>프로필 이미지</h3>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div>
+            <h3>
+              제공 서비스<span>*</span>
+            </h3>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
