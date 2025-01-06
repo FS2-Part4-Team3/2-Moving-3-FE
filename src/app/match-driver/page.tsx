5b1fb869ee1f7cf6cac4d1a6092310087649c8f5
hyobiin9 <moone1234@kakao.com>
2025-01-06 15:15:33 +0900

    style: overall design exception driver card

--- src/app/match-driver/page.tsx
+++ src/app/match-driver/page.tsx
@@ -4,14 +4,22 @@ import SortDropdown from "@/components/dropdown/SortDropdown";
 
 export default function MatchDriver() {
   return (
-    <div>
-      <p>기사님 찾기</p>
-      <div>
-        <RegionServiceDropdown />
-        <div>
-          <SortDropdown />
-          <SearchBar />
-          <h1>Driver Card</h1>
+    <div className="w-full flex items-center justify-center">
+      <div className="w-[120rem] items-start justify-center flex flex-col">
+        <div className="py-[3.2rem]">
+          <p className="font-semibold text-[2.4rem] leading-[3.2rem] text-[#2B2B2B]">
+            기사님 찾기
+          </p>
+        </div>
+        <div className="flex gap-[10rem] w-full">
+          <RegionServiceDropdown />
+          <div className="flex flex-col items-center gap-[3.2rem] flex-grow max-w-[calc(100%-10rem)]">
+            <div className="w-full flex flex-col gap-[2.4rem] items-end">
+              <SortDropdown />
+              <SearchBar />
+            </div>
+            <h1>Driver Card</h1>
+          </div>
         </div>
       </div>
     </div>
