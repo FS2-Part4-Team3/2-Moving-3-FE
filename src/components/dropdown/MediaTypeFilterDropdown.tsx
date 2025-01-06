26bbbd6300dad8116f9223c959d67926f2aff4b0
hyobiin9 <moone1234@kakao.com>
2025-01-06 18:18:09 +0900

    feat: filter page

--- src/components/dropdown/MediaTypeFilterDropdown.tsx
+++ src/components/dropdown/MediaTypeFilterDropdown.tsx
@@ -15,6 +15,8 @@ export default function MediaTypeFilterDropdown() {
   const [serviceable, setServiceable] = useState<boolean>(false);
   const [appointRequest, setAppointRequest] = useState<boolean>(false);
 
+  const [isMenuClick, setIsMenuClick] = useState<"mov" | "filter">("mov");
+
   const handleInquiryClick = () => {
     console.log("눌렸습니다");
   };
@@ -147,42 +149,150 @@ export default function MediaTypeFilterDropdown() {
         </div>
       </div>
       <div className="w-full min-h-screen flex items-center justify-center bg-black bg-opacity-50 lg:hidden">
-        <div className="bg-white w-[37.5rem] rounded-[3.2rem] pt-[1.6rem] pb-[3.2rem] px-[2.4rem] gap-[2.4rem]">
+        <div className="bg-white w-[37.5rem] rounded-[3.2rem] pt-[1.6rem] pb-[3.2rem] px-[2.4rem] flex flex-col gap-[2.4rem]">
           <div className="flex flex-col gap-[1.2rem]">
             <div className="flex w-full justify-between p-[0.8rem]">
               <div className="flex gap-[2.4rem]">
-                <p className="font-bold text-[1.8rem] leading-[2.6rem] text-black-400">
+                <p
+                  className="font-bold text-[1.8rem] leading-[2.6rem] text-black-400 cursor-pointer"
+                  onClick={() => setIsMenuClick("mov")}
+                >
                   이사 유형
                 </p>
-                <p className="font-bold text-[1.8rem] leading-[2.6rem] text-black-400">
+                <p
+                  className="font-bold text-[1.8rem] leading-[2.6rem] text-black-400 cursor-pointer"
+                  onClick={() => setIsMenuClick("filter")}
+                >
                   필터
                 </p>
               </div>
               <Image src={x} alt="x" width={24} height={24} />
             </div>
-            <div className="flex flex-col gap-[0.8rem]">
-              <div className="flex gap-[1rem] items-center py-[0.8rem] px-[1rem] border-b border-line-100 justify-between">
-                <div className="flex">
-                  <p className="font-normal text-[1.6rem] leading-[2.6rem] text-gray-300">
-                    전체선택 (199999)
-                  </p>
+            {isMenuClick === "mov" && (
+              <div className="flex flex-col gap-[0.8rem]">
+                <div className="flex gap-[1rem] items-center py-[0.8rem] px-[1rem] border-b border-line-100 justify-between">
+                  <div className="flex">
+                    <p className="font-normal text-[1.6rem] leading-[2.6rem] text-gray-300">
+                      전체선택 (199999)
+                    </p>
+                  </div>
+                  <Image
+                    src={
+                      smallMov && homeMov && officeMov
+                        ? checkbox_blue
+                        : checkbox
+                    }
+                    alt="checkbox"
+                    width={36}
+                    height={36}
+                    onClick={() => {
+                      setSmallMov(!smallMov);
+                      setHomeMov(!homeMov);
+                      setOfficeMov(!officeMov);
+                    }}
+                    className="cursor-pointer"
+                  />
                 </div>
-                <Image
-                  src={
-                    smallMov && homeMov && officeMov ? checkbox_blue : checkbox
-                  }
-                  alt="checkbox"
-                  width={36}
-                  height={36}
-                  onClick={() => {
-                    setSmallMov(!smallMov);
-                    setHomeMov(!homeMov);
-                    setOfficeMov(!officeMov);
-                  }}
-                  className="cursor-pointer"
-                />
+                <div>
+                  <div className="flex justify-between border-b border-line-100 p-[1.6rem]">
+                    <div className="flex gap-[0.4rem] font-medium text-[1.6rem] leading-[2.6rem] text-black-400">
+                      <p>소형이사</p>
+                      <p>(10)</p>
+                    </div>
+                    <Image
+                      src={smallMov ? checkbox_blue : checkbox}
+                      alt="checkbox"
+                      width={36}
+                      height={36}
+                      onClick={() => setSmallMov(!smallMov)}
+                      className="cursor-pointer"
+                    />
+                  </div>
+                  <div className="flex justify-between border-b border-line-100 p-[1.6rem]">
+                    <div className="flex gap-[0.4rem] font-medium text-[1.6rem] leading-[2.6rem] text-black-400">
+                      <p>가정이사</p>
+                      <p>(50)</p>
+                    </div>
+                    <Image
+                      src={homeMov ? checkbox_blue : checkbox}
+                      alt="checkbox"
+                      width={36}
+                      height={36}
+                      onClick={() => setHomeMov(!homeMov)}
+                      className="cursor-pointer"
+                    />
+                  </div>
+                  <div className="flex justify-between border-b border-line-100 p-[1.6rem]">
+                    <div className="flex gap-[0.4rem] font-medium text-[1.6rem] leading-[2.6rem] text-black-400">
+                      <p>사무실이사</p>
+                      <p>(1999)</p>
+                    </div>
+                    <Image
+                      src={officeMov ? checkbox_blue : checkbox}
+                      alt="checkbox"
+                      width={36}
+                      height={36}
+                      onClick={() => setOfficeMov(!officeMov)}
+                      className="cursor-pointer"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
+            )}
+            {isMenuClick === "filter" && (
+              <div className="flex flex-col gap-[0.8rem]">
+                <div className="flex gap-[1rem] items-center py-[0.8rem] px-[1rem] border-b border-line-100 justify-between">
+                  <div className="flex">
+                    <p className="font-normal text-[1.6rem] leading-[2.6rem] text-gray-300">
+                      전체선택 (18888)
+                    </p>
+                  </div>
+                  <Image
+                    src={
+                      serviceable && appointRequest ? checkbox_blue : checkbox
+                    }
+                    alt="checkbox"
+                    width={36}
+                    height={36}
+                    onClick={() => {
+                      setSmallMov(!serviceable);
+                      setHomeMov(!appointRequest);
+                    }}
+                    className="cursor-pointer"
+                  />
+                </div>
+                <div>
+                  <div className="flex justify-between border-b border-line-100 p-[1.6rem]">
+                    <div className="flex gap-[0.4rem] font-medium text-[1.6rem] leading-[2.6rem] text-black-400">
+                      <p>서비스 가능 지역</p>
+                      <p>(10)</p>
+                    </div>
+                    <Image
+                      src={smallMov ? checkbox_blue : checkbox}
+                      alt="checkbox"
+                      width={36}
+                      height={36}
+                      onClick={() => setSmallMov(!smallMov)}
+                      className="cursor-pointer"
+                    />
+                  </div>
+                  <div className="flex justify-between border-b border-line-100 p-[1.6rem]">
+                    <div className="flex gap-[0.4rem] font-medium text-[1.6rem] leading-[2.6rem] text-black-400">
+                      <p>지정 견적 요청</p>
+                      <p>(50)</p>
+                    </div>
+                    <Image
+                      src={homeMov ? checkbox_blue : checkbox}
+                      alt="checkbox"
+                      width={36}
+                      height={36}
+                      onClick={() => setHomeMov(!homeMov)}
+                      className="cursor-pointer"
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
           <ButtonWrapper id="inquiry-driver" onClick={handleInquiryClick}>
             <ButtonWrapper.Button className="w-full h-[5.4rem] rounded-[1.6rem] p-[1.6rem] font-semibold text-[1.6rem] leading-[2.6rem] text-white">
