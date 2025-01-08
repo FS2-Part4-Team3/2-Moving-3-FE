126d5c7fe7a7fbacd7ccc547d2d66dacb1c3b27c
hyobiin9 <moone1234@kakao.com>
2025-01-08 10:57:30 +0900

    style: receive quote card design

--- src/components/cards/ReceiveQuoteCard.tsx
+++ src/components/cards/ReceiveQuoteCard.tsx
@@ -6,40 +6,58 @@ import Image from "next/image";
 
 export default function ReceiveQuoteCard({ data }: ReceiveQuoteCardProps) {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col max-w-[95.5rem] border border-line-100 pt-[2rem] pb-[2.4rem] px-[2.4rem] shadow-[-0.2rem_-0.2rem_1rem_rgba(220,220,220,0.14)] rounded-[1.6rem] gap-[1.6rem]">
+      <div className="flex w-full justify-between items-center">
         <MovingTypeChips type={data.type} />
-        <p>{data.updatedAt}</p>
+        <p className="font-normal text-[1.4rem] leading-[2.4rem] text-gray-500">
+          {data.updatedAt}
+        </p>
       </div>
-      <div>
-        <p>{data.owner} 고객님</p>
-        <div />
-        <div>
-          <div>
-            <p>이사일</p>
-            <p>{data.date}</p>
+      <div className="flex flex-col gap-[1.8rem]">
+        <p className="font-semibold text-[2rem] leading-[3.2rem] text-black-300">
+          {data.owner} 고객님
+        </p>
+        <div className="w-full border border-line-200" />
+        <div className="flex gap-[1.6rem] items-center">
+          <div className="flex gap-[1.2rem] items-center">
+            <p className="rounded-[0.4rem] py-[0.4rem] px-[0.6rem] bg-background-400 w-fit h-fit font-normal text-[1.8rem] leading-[2.6rem] text-gray-500">
+              이사일
+            </p>
+            <p className="font-medium text-[1.8rem] leading-[2.6rem] text-black-300">
+              {data.date}
+            </p>
           </div>
-          <div />
-          <div>
-            <p>출발</p>
-            <p>{data.fromAddress}</p>
+          <div className="h-[1.6rem] border border-line-200" />
+          <div className="flex gap-[1.2rem] items-center">
+            <p className="rounded-[0.4rem] py-[0.4rem] px-[0.6rem] bg-background-400 w-fit h-fit font-normal text-[1.8rem] leading-[2.6rem] text-gray-500">
+              출발
+            </p>
+            <p className="font-medium text-[1.8rem] leading-[2.6rem] text-black-300">
+              {data.fromAddress}
+            </p>
           </div>
-          <div />
-          <div>
-            <p>도착</p>
-            <p>{data.toAddress}</p>
+          <div className="h-[1.6rem] border border-line-200" />
+          <div className="flex gap-[1.2rem] items-center">
+            <p className="rounded-[0.4rem] py-[0.4rem] px-[0.6rem] bg-background-400 w-fit h-fit font-normal text-[1.8rem] leading-[2.6rem] text-gray-500">
+              도착
+            </p>
+            <p className="font-medium text-[1.8rem] leading-[2.6rem] text-black-300">
+              {data.toAddress}
+            </p>
           </div>
         </div>
       </div>
-      <div>
+      <div className="w-full flex justify-between">
         <ButtonWrapper id="send-quote-button">
-          <ButtonWrapper.Button>
+          <ButtonWrapper.Button className="w-[44.8rem] h-[6.4rem] rounded-[1.6rem] p-[1.6rem] flex items-center justify-center gap-[1rem] font-semibold text-[2rem] leading-[3.2rem] text-white">
             견적 보내기
             <Image src={writing} alt="writing" width={24} height={24} />
           </ButtonWrapper.Button>
         </ButtonWrapper>
         <ButtonWrapper id="return-button">
-          <ButtonWrapper.Button>반려</ButtonWrapper.Button>
+          <ButtonWrapper.Button className="w-[44.8rem] h-[6.4rem] rounded-[1.6rem] p-[1.6rem] flex items-center justify-center py-[1.6rem] px-[2.4rem] border border-blue-300 bg-white text-blue-300 font-semibold text-[2rem] leading-[3.2rem]">
+            반려
+          </ButtonWrapper.Button>
         </ButtonWrapper>
       </div>
     </div>
