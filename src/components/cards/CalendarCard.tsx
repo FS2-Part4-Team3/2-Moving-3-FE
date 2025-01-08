73fd3b25bd9b4c5a0aa2abacf616adaea471410d
unknown <d653@naver.com>
2025-01-08 10:15:41 +0900

    Feat:#44 - responsive design

--- src/components/cards/CalendarCard.tsx
+++ src/components/cards/CalendarCard.tsx
@@ -78,41 +78,41 @@ export default function CalendarCard() {
   const daysInMonth = getDaysInMonth(currentMonth);
 
   return (
-    <div className="lg:w-[64rem]  flex flex-col items-center rounded-[3.2rem] py-[2.4rem] gap-[2.4rem] bg-white border-none">
-      <div className="flex lg:w-[64rem] lg:h-[6rem] justify-between items-center px-[1.2rem] py-[1.4rem]">
+    <div className="lg:w-[64rem] md:w-[32.7rem] sm:w-[32.7rem] flex flex-col items-center lg:rounded-[3.2rem] md:rounded-[1.6rem] sm:rounded-[1.6rem] lg:py-[2.4rem] md:py-[1.4rem] sm:py-[1.4rem] lg:gap-[2.4rem] md:gap-[1.6rem] sm:gap-[1.6rem] bg-white border-none">
+      <div className="flex lg:w-[64rem] md:w-[32.7rem] sm:w-[32.7rem] lg:h-[6rem] md:h-[4.8rem] sm:h-[4.8rem] justify-between items-center px-[1.4rem] lg:py-[1.2rem] md:py-[1rem] sm:py-[1rem]">
         <div
-          className="relative lg:w-[3.6rem] lg:h-[3.6rem] cursor-pointer"
+          className="relative lg:w-[3.6rem] lg:h-[3.6rem] md:w-[2.4rem] md:h-[2.4rem] sm:w-[2.4rem] sm:h-[2.4rem] cursor-pointer"
           onClick={handlePrev}
         >
           <Image src={left} alt="이전 달" fill />
         </div>
-        <p className="text-[2rem] font-semibold ">
+        <p className="lg:text-[2rem] md:text-[1.6rem] sm:text-[1.6rem] text-black-400 font-semibold ">
           {currentMonth.getFullYear()}.
           {String(currentMonth.getMonth() + 1).padStart(2, "0")}
         </p>
         <div
-          className="relative lg:w-[3.6rem] lg:h-[3.6rem] cursor-pointer"
+          className="relative lg:w-[3.6rem] lg:h-[3.6rem] md:w-[2.4rem] md:h-[2.4rem] sm:w-[2.4rem] sm:h-[2.4rem] cursor-pointer"
           onClick={handleNext}
         >
           <Image src={right} alt="다음 달" fill />
         </div>
       </div>
-      <div className="grid grid-cols-7 text-center lg:w-[64rem] lg:h-[6.8rem] py-[0.2rem] px-[3.6rem] justify-items-center">
+      <div className="grid grid-cols-7 text-center lg:w-[64rem] md:w-[32.7rem] sm:w-[32.7rem] lg:h-[6.8rem] py-[0.2rem] lg:px-[3.6rem] md:px-[0.9rem] sm:px-[0.9rem] justify-items-center">
         {weekdays.map((day, index) => (
           <div
             key={index}
-            className="w-[4rem] h-[4rem] text-[2rem] font-medium text-gray-500 flex justify-center items-center"
+            className="lg:w-[4rem] lg:h-[4rem] md:w-[2.6rem] md:h-[2.6rem] sm:w-[2.6rem] sm:h-[2.6rem] lg:text-[2rem] md:text-[1.3rem] sm:text-[1.3rem] font-medium text-gray-500 flex justify-center items-center"
           >
             {day.name}
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-7 text-center lg:w-[64rem] py-[0.2rem] px-[3.6rem] justify-items-center gap-y-[3rem]">
+      <div className="grid grid-cols-7 text-center lg:w-[64rem] md:w-[32.7rem] sm:w-[32.7rem] py-[0.2rem] lg:px-[3.6rem] md:px-[0.9rem] sm:px-[0.9rem] justify-items-center lg:gap-y-[3rem] md:gap-y-[1.2rem] sm:gap-y-[1.2rem]">
         {daysInMonth.map((day, index) => (
           <div
             key={index}
             onClick={() => handleDateClick(day)}
-            className={`cursor-pointer text-[2rem] font-medium w-[4rem] h-[4rem] flex justify-center items-center ${
+            className={`lg:w-[4rem] lg:h-[4rem] md:w-[2.6rem] md:h-[2.6rem] sm:w-[2.6rem] sm:h-[2.6rem] lg:text-[2rem] md:text-[1.3rem] sm:text-[1.3rem] cursor-pointer flex justify-center items-center ${
               day.isCurrentMonth
                 ? selectedDate &&
                   selectedDate.getDate() === day.date &&
@@ -130,7 +130,7 @@ export default function CalendarCard() {
         <ButtonWrapper.Button
           disabled={!selectedDate}
           onClick={handleSelectComplte}
-          className="lg:w-[56rem] lg:h-[6.4rem] rounded-[1.6rem] p-[1.6rem] text-center text-[2rem] font-semibold text-white"
+          className="lg:w-[56rem] lg:h-[6.4rem] md:w-[27.9rem] md:h-[5.4rem] sm:w-[27.9rem] sm:h-[5.4rem] rounded-[1.6rem] p-[1.6rem] text-center lg:text-[2rem] md:text-[1.6rem] sm:text-[1.6rem] font-semibold text-white"
         >
           선택완료
         </ButtonWrapper.Button>
