958a64d9d75b2b40a962dc0d10129ba25a413e8f
hyobiin9 <moone1234@kakao.com>
2025-01-09 18:13:29 +0900

    feat: waiting quote tab initial structure

--- src/components/Tabs/WaitingQuoteTab.tsx
+++ src/components/Tabs/WaitingQuoteTab.tsx
@@ -1,11 +1,11 @@
 export default function WaitingQuoteTab() {
   return (
-    <div>
-      <div>
-        <p>대기 중인 견적</p>
+    <div className="flex gap-[3.2rem] pt-[1.6rem]">
+      <div className="border-b-2 border-black-400">
+        <p className="font-semibold text-[2rem] leading-[3.2rem] text-[#2B2B2B]">대기 중인 견적</p>
       </div>
-      <div>
-        <p>받았던 견적</p>
+      <div className="border-b-2 border-black-400">
+        <p className="font-semibold text-[2rem] leading-[3.2rem] text-[#2B2B2B]">받았던 견적</p>
       </div>
     </div>
   );
