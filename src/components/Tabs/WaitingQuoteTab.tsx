83bfdba1b6d4d06ba86cf773b04bfb8ffd95f98c
hyobiin9 <moone1234@kakao.com>
2025-01-09 18:18:29 +0900

    feat: waiting quote tab click event

--- src/components/Tabs/WaitingQuoteTab.tsx
+++ src/components/Tabs/WaitingQuoteTab.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 export default function WaitingQuoteTab() {
+  const [selectTab, setSelectTab] = useState<'wait' | 'estimate'>('wait');
   return (
     <div className="flex gap-[3.2rem] pt-[1.6rem]">
-      <div className="border-b-2 border-black-400">
-        <p className="font-semibold text-[2rem] leading-[3.2rem] text-[#2B2B2B]">대기 중인 견적</p>
+      <div
+        className={`py-[1.6rem] cursor-pointer ${selectTab === 'wait' ? 'border-b-2 border-black-400' : ''}`}
+        onClick={() => setSelectTab('wait')}
+      >
+        <p className={`font-semibold text-[2rem] leading-[3.2rem] ${selectTab === 'wait' ? 'text-[#2B2B2B]' : 'text-gray-400'}`}>
+          대기 중인 견적
+        </p>
       </div>
-      <div className="border-b-2 border-black-400">
-        <p className="font-semibold text-[2rem] leading-[3.2rem] text-[#2B2B2B]">받았던 견적</p>
+      <div
+        className={`py-[1.6rem] cursor-pointer ${selectTab === 'estimate' ? 'border-b-2 border-black-400' : ''}`}
+        onClick={() => setSelectTab('estimate')}
+      >
+        <p
+          className={`font-semibold text-[2rem] leading-[3.2rem] ${selectTab === 'estimate' ? 'text-[#2B2B2B]' : 'text-gray-400'}`}
+        >
+          받았던 견적
+        </p>
       </div>
     </div>
   );
