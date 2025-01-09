e35c3e108b75a43d134b4f606cda057bf33db407
hyobiin9 <moone1234@kakao.com>
2025-01-09 16:10:02 +0900

    fix: eslint file name

--- src/components/common/headless/Button.tsx
+++ src/components/common/headless/Button.tsx
@@ -1,27 +1,18 @@
-import { createContext, useContext } from "react";
-import type { ButtonContextType } from "@/interfaces/CommonComp/HeadlessInterface";
+import { createContext, useContext } from 'react';
+import type { ButtonContextType } from '@/interfaces/CommonComp/HeadlessInterface';
 
 const ButtonContext = createContext<ButtonContextType | undefined>(undefined);
 
-export const ButtonWrapper = ({
-  children,
-  id,
-  onClick,
-  type,
-}: ButtonContextType & { children: React.ReactNode }) => {
+export const ButtonWrapper = ({ children, id, onClick, type }: ButtonContextType & { children: React.ReactNode }) => {
   const contextValue = { id, onClick, type };
 
-  return (
-    <ButtonContext.Provider value={contextValue}>
-      {children}
-    </ButtonContext.Provider>
-  );
+  return <ButtonContext.Provider value={contextValue}>{children}</ButtonContext.Provider>;
 };
 
 const useButtonContext = () => {
   const context = useContext(ButtonContext);
   if (!context) {
-    throw new Error("useButtonContext must be used within a ButtonWrapper");
+    throw new Error('useButtonContext must be used within a ButtonWrapper');
   }
   return context;
 };
@@ -31,7 +22,7 @@ const Button = ({
   disabled = false,
   children,
   ...props
-}: React.ComponentPropsWithoutRef<"button"> & {
+}: React.ComponentPropsWithoutRef<'button'> & {
   className?: string;
   disabled?: boolean;
 }) => {
@@ -41,9 +32,7 @@ const Button = ({
     <button
       id={id}
       onClick={disabled ? undefined : onClick}
-      className={`${className} ${
-        disabled ? "cursor-not-allowed bg-gray-100" : "bg-blue-300"
-      }`}
+      className={`${className} ${disabled ? 'cursor-not-allowed bg-gray-100' : 'bg-blue-300'}`}
       disabled={disabled}
       {...props}
     >
