5215a15b99906a024b4531550738e2a6e93c5c12
hyobiin9 <moone1234@kakao.com>
2025-01-09 18:32:10 +0900

    feat: date utils include time

--- src/utils/Format.ts
+++ src/utils/Format.ts
@@ -1,20 +1,37 @@
 export default function AddressFormat(address: string) {
   const match = address.match(/^[^\s]+\s[^\s]+/);
-  const result = match ? match[0] : "";
+  const result = match ? match[0] : '';
   return result;
 }
 
 export function DateFormat(isoDate: string): string {
-  const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
   const date = new Date(isoDate);
 
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
 
   const dayOfWeek = daysOfWeek[date.getDay()];
 
   return `${year}. ${month}. ${day}(${dayOfWeek})`;
+}
+
+export function DateIncludeTimeFormat(isoString: string) {
+  const days = ['일', '월', '화', '수', '목', '금', '토'];
+  const date = new Date(isoString);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const dayOfWeek = days[date.getDay()];
+
+  const hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const period = hours < 12 ? '오전' : '오후';
+  const formattedHours = hours % 12 || 12;
+
+  return `${year}. ${month}. ${day}(${dayOfWeek}) ${period} ${formattedHours}:${minutes}`;
 }
 
 export function timeAgoFormat(isoDate: string): string {
