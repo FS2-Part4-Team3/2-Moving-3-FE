0bdf9e0a352738f8fbf006eb94b1933f629716f0
hyobiin9 <moone1234@kakao.com>
2025-01-09 17:24:33 +0900

    style: waiting quote card css

--- src/components/cards/WaitingQuoteCard.tsx
+++ src/components/cards/WaitingQuoteCard.tsx
@@ -13,10 +13,10 @@ export default function WaitingQuoteCard({ data }: WaitingQuoteCardProps) {
           <MovingTypeChips type="WAITING" />
           <MovingTypeChips type={data.moveInfo.type} />
         </div>
-        <div className="flex gap-[2.4rem]">
+        <div className="flex flex-col gap-[2.4rem]">
           <div className="flex w-full gap-[2.4rem] py-[1.6rem] px-[1.8rem] border border-line-100 rounded-[0.6rem] shadow-[-0.2rem_-0.2rem_1rem_rgba(220,220,220,0.14)]">
             <Image src={data.driver.image} alt="profile" width={56} height={56} />
-            <div className="w-full flex flex-col items-center">
+            <div className="w-full flex flex-col items-center gap-[0.8rem]">
               <div className="w-full flex justify-between items-center">
                 <p className="font-semibold text-[1.8rem] leading-[2.6rem] text-black-300">{data.driver.name}</p>
                 <div className="flex gap-[0.4rem] items-center">
@@ -24,44 +24,54 @@ export default function WaitingQuoteCard({ data }: WaitingQuoteCardProps) {
                   <p className="font-medium text-[1.8rem] leading-[2.6rem] text-blue-400">{data.driver.favoriteCount}</p>
                 </div>
               </div>
-              <div>
-                <div>
+              <div className="flex gap-[1.6rem] items-start w-full">
+                <div className="flex gap-[0.6rem] items-center">
                   <Image src={star} alt="star" width={24} height={24} />
-                  <p>{data.driver.score}</p>
-                  <p>({data.driver.reviewCount})</p>
+                  <p className="font-medium text-[1.6rem] leading-[2.6rem] text-black-300">{data.driver.score}</p>
+                  <p className="font-medium text-[1.6rem] leading-[2.6rem] text-gray-300">({data.driver.reviewCount})</p>
                 </div>
-                <div />
-                <div>
-                  <p>경력</p>
-                  <p>{data.driver.career}</p>
+                <div className="h-[1.4rem] border border-line-200" />
+                <div className="flex gap-[0.6rem]">
+                  <p className="font-medium text-[1.6rem] leading-[2.6rem] text-gray-300">경력</p>
+                  <p className="font-medium text-[1.6rem] leading-[2.6rem] text-black-300">{data.driver.career}년</p>
                 </div>
-                <div />
-                <div>
-                  <p>{data.driver.applyCount}건</p>
-                  <p>확정</p>
+                <div className="h-[1.4rem] border border-line-200" />
+                <div className="flex gap-[0.6rem]">
+                  <p className="font-medium text-[1.6rem] leading-[2.6rem] text-black-300">{data.driver.applyCount}건</p>
+                  <p className="font-medium text-[1.6rem] leading-[2.6rem] text-gray-300">확정</p>
                 </div>
               </div>
             </div>
           </div>
-          <div>
-            <div>
-              <p>이사일</p>
-              <p>{DateFormat(data.moveInfo.date)}</p>
+          <div className="flex gap-[1.6rem]">
+            <div className="flex gap-[1.2rem] items-center">
+              <p className="rounded-[0.4rem] py-[0.4rem] px-[0.6rem] bg-background-400 font-normal text-[1.8rem] leading-[2.6rem] text-gray-500">
+                이사일
+              </p>
+              <p className="font-medium text-[1.8rem] leading-[2.6rem] text-black-300">{DateFormat(data.moveInfo.date)}</p>
             </div>
-            <div>
-              <p>출발</p>
-              <p>{AddressFormat(data.moveInfo.fromAddress)}</p>
+            <div className="flex gap-[1.2rem] items-center">
+              <p className="rounded-[0.4rem] py-[0.4rem] px-[0.6rem] bg-background-400 font-normal text-[1.8rem] leading-[2.6rem] text-gray-500">
+                출발
+              </p>
+              <p className="font-medium text-[1.8rem] leading-[2.6rem] text-black-300">
+                {AddressFormat(data.moveInfo.fromAddress)}
+              </p>
             </div>
-            <div>
-              <p>도착</p>
-              <p>{AddressFormat(data.moveInfo.toAddress)}</p>
+            <div className="flex gap-[1.2rem] items-center">
+              <p className="rounded-[0.4rem] py-[0.4rem] px-[0.6rem] bg-background-400 font-normal text-[1.8rem] leading-[2.6rem] text-gray-500">
+                도착
+              </p>
+              <p className="font-medium text-[1.8rem] leading-[2.6rem] text-black-300">
+                {AddressFormat(data.moveInfo.toAddress)}
+              </p>
             </div>
           </div>
         </div>
       </div>
-      <div>
-        <p>견적 금액</p>
-        <p>{data.price}</p>
+      <div className="w-full flex justify-end items-center gap-[1.6rem]">
+        <p className="font-medium text-[1.8rem] leading-[2.6rem] text-black-400">견적 금액</p>
+        <p className="font-bold text-[2.4rem] leading-[3.2rem] text-black-400">{data.price}</p>
       </div>
       <div>
         <p>button</p>
