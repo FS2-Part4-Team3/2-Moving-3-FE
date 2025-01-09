0e525404a119048fb0508fc4041d46cf894ec510
hyobiin9 <moone1234@kakao.com>
2025-01-09 18:53:32 +0900

    style: estimation information card css

--- src/components/cards/EstimateInformationCard.tsx
+++ src/components/cards/EstimateInformationCard.tsx
@@ -10,17 +10,17 @@ enum MoveType {
 
 export default function EstimationInformationCard({ data }: EstimationInformationCardProps) {
   return (
-    <div>
-      <p>견적 정보</p>
-      <div>
-        <div>
+    <div className="flex flex-col gap-[4rem]">
+      <p className="font-semibold text-[2.4rem] leading-[3.2rem] text-black-400">견적 정보</p>
+      <div className="rounded-[1.6rem] border border-line-100 py-[3.2rem] px-[4rem] bg-background-100 flex gap-[3.2rem]">
+        <div className="flex flex-col gap-[0.8rem] font-normal text-[2rem] leading-[3.2rem] text-gray-300">
           <p>견적 요청일</p>
           <p>서비스</p>
           <p>이용일</p>
           <p>출발지</p>
           <p>도착지</p>
         </div>
-        <div>
+        <div className="flex flex-col gap-[0.8rem] font-normal text-[2rem] leading-[3.2rem] text-black-400">
           <p>{DateWithoutDayWeeKFormat(data.updatedAt)}</p>
           <p>{MoveType[data.moveInfo.type]}</p>
           <p>{DateIncludeTimeFormat(data.moveInfo.date)}</p>
