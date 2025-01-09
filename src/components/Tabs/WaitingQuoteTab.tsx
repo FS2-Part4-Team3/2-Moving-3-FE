cad34af4a531bb212535b106e7cc9201901867f6
hyobiin9 <moone1234@kakao.com>
2025-01-09 18:10:21 +0900

    feat: waiting quote tab storybook

---
+++ src/components/Tabs/WaitingQuoteTab.tsx
@@ -0,0 +1,12 @@
+export default function WaitingQuoteTab() {
+  return (
+    <div>
+      <div>
+        <p>대기 중인 견적</p>
+      </div>
+      <div>
+        <p>받았던 견적</p>
+      </div>
+    </div>
+  );
+}
