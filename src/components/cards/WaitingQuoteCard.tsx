1df7dfeb5db847f4b8534593efdd507795cac647
hyobiin9 <moone1234@kakao.com>
2025-01-09 16:28:49 +0900

    fix: waiting quote card storybook

--- src/components/cards/WaitingQuoteCard.tsx
+++ src/components/cards/WaitingQuoteCard.tsx
@@ -1,4 +1,6 @@
-export default function WaitingQuoteCard() {
+import type { WaitingQuoteCardProps } from '@/interfaces/Card/WaitingQuoteCardInterface';
+
+export default function WaitingQuoteCard({ data }: WaitingQuoteCardProps) {
   return (
     <div className="rounded-[1.6rem] border border-line-100 pt-[2.8rem] pb-[2.2rem] px-[2.4rem] gap-[2.4rem] shadow-[-0.2rem_-0.2rem_1rem_rgba(220,220,220,0.14)] shadow-[0.2rem_0.2rem_1rem_rgba(220,220,220,0.14)]">
       <div>Profile</div>
