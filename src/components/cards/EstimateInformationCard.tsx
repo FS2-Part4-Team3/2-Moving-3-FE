41a628783a33c5bfe67d8b301c319fe06976127f
hyobiin9 <moone1234@kakao.com>
2025-01-09 20:02:54 +0900

    feat: estimate information card media query

--- src/components/cards/EstimateInformationCard.tsx
+++ src/components/cards/EstimateInformationCard.tsx
@@ -10,17 +10,19 @@ enum MoveType {
 
 export default function EstimationInformationCard({ data }: EstimationInformationCardProps) {
   return (
-    <div className="flex flex-col gap-[4rem]">
-      <p className="font-semibold text-[2.4rem] leading-[3.2rem] text-black-400">견적 정보</p>
-      <div className="rounded-[1.6rem] border border-line-100 py-[3.2rem] px-[4rem] bg-background-100 flex gap-[3.2rem]">
-        <div className="flex flex-col gap-[0.8rem] font-normal text-[2rem] leading-[3.2rem] text-gray-300">
+    <div className="flex flex-col lg:gap-[4rem] sm:gap-[2.4rem]">
+      <p className="font-semibold lg:text-[2.4rem] lg:leading-[3.2rem] sm:text-[1.6rem] sm:leading-[2.6rem] text-black-400">
+        견적 정보
+      </p>
+      <div className="rounded-[1.6rem] border border-line-100 md:py-[3.2rem] md:px-[4rem] sm:p-[1.6rem] bg-background-100 flex lg:gap-[3.2rem] gap-[4rem]">
+        <div className="flex flex-col gap-[0.8rem] font-normal lg:text-[2rem] lg:leading-[3.2rem] sm:text-[1.4rem] sm:leading-[2.4rem] text-gray-300">
           <p>견적 요청일</p>
           <p>서비스</p>
           <p>이용일</p>
           <p>출발지</p>
           <p>도착지</p>
         </div>
-        <div className="flex flex-col gap-[0.8rem] font-normal text-[2rem] leading-[3.2rem] text-black-400">
+        <div className="flex flex-col gap-[0.8rem] font-normal lg:text-[2rem] lg:leading-[3.2rem] sm:text-[1.4rem] sm:leading-[2.4rem] text-black-400">
           <p>{DateWithoutDayWeeKFormat(data.updatedAt)}</p>
           <p>{MoveType[data.moveInfo.type]}</p>
           <p>{DateIncludeTimeFormat(data.moveInfo.date)}</p>
