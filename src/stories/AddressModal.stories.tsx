de6bb824898260ce0e9a01aefe3d4f0ed1f68789
unknown <d653@naver.com>
2025-01-10 10:38:52 +0900

    Fix:#55 - address modal storybook

--- src/stories/AddressModal.stories.tsx
+++ src/stories/AddressModal.stories.tsx
@@ -1,10 +1,32 @@
-import AddressModal from "@/components/modal/AddressModal";
+import type { StoryFn } from '@storybook/react';
+import { useState } from 'react';
+import AddressModal from '@/components/modal/AddressModal';
+import type { AddressModalProps } from '@/interfaces/Modal/AddressModalInterface';
 
 export default {
-  title: "Components/Modals/AddressModal",
+  title: 'Components/Modals/AddressModal',
   component: AddressModal,
+  parameters: {
+    layout: 'fullscreen',
+  },
 };
 
-const Template = () => <AddressModal />;
+const Template: StoryFn<AddressModalProps> = args => {
+  const [regions, setRegions] = useState({ start: '', arrival: '' });
 
-export const Default = Template.bind({});
+  return <AddressModal {...args} setRegions={setRegions} />;
+};
+
+export const StartModalOpen = Template.bind({});
+StartModalOpen.args = {
+  handleModalClose: () => alert('Modal closed'),
+  isArrivalModalOpen: false,
+  isStartModalOpen: true,
+};
+
+export const ArrivalModalOpen = Template.bind({});
+ArrivalModalOpen.args = {
+  handleModalClose: () => alert('Modal closed'),
+  isArrivalModalOpen: true,
+  isStartModalOpen: false,
+};
