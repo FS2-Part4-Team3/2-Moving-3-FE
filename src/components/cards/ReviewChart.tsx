532ff122664a0f9f80bbc0c99d187cdbdbff2c17
Jaewon Kim <magry78@gmail.com>
2025-01-13 15:56:19 +0900

    fix:#46-PR comment reflect

--- src/components/cards/ReviewChart.tsx
+++ src/components/cards/ReviewChart.tsx
@@ -64,7 +64,7 @@ export default function ReviewChart({ data, score, reviewCount }: ReviewChartPro
                 <div
                   className="bg-yellow-100 h-full rounded-[1.5rem]"
                   style={{
-                    width: `${reviewCount > 0 ? (item.count / reviewCount) * 100 : 0}%`,
+                    width: `${reviewCount ? (item.count / reviewCount) * 100 : 0}%`,
                   }}
                 ></div>
               </div>
