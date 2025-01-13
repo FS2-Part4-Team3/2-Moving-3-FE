a349ca2a96281df5ccd1ef8f317fb1a09a7a506a
hyobiin9 <moone1234@kakao.com>
2025-01-13 20:46:54 +0900

    feat: my quote received page initial structue

--- src/app/normal/my-quote/received/page.tsx
+++ src/app/normal/my-quote/received/page.tsx
@@ -1,4 +1,8 @@
+import EstimateData from '@/../public/data/estimationsData.json';
 import WaitingQuoteTab from '@/components/Tabs/WaitingQuoteTab';
+import EstimationInformationCard from '@/components/cards/EstimateInformationCard';
+import EstimateReceivedCard from '@/components/cards/EstimateReceivedCard';
+import EstimationSortDropdown from '@/components/dropdown/EstimationSortDropdown';
 
 export default function MyQuoteReceived() {
   return (
@@ -9,8 +13,19 @@ export default function MyQuoteReceived() {
         </div>
       </div>
       <div className="lg:min-w-[119rem] lg:max-w-[140rem] sm:w-full justify-center flex flex-col mt-[4rem]">
-        <div className="bg-white rounded-[4rem] py-[4.8rem] px-[4rem] gap-[4.8rem] border border-line-100 shadow-[-0.2rem_-0.2rem_1rem_rgba(220,220,220,0.3)] shadow-[0.2rem_0.2rem_1rem_rgba(220,220,220,0.3)]">
-          {/* Estimation Card 추가부터 */}
+        <div className="bg-white rounded-[4rem] py-[4.8rem] px-[4rem] flex flex-col gap-[4.8rem] border border-line-100 shadow-[0.2rem_-0.2rem_1rem_rgba(220,220,220,0.3)]">
+          <EstimationInformationCard data={EstimateData[0]} />
+          <div className="flex flex-col gap-[4rem]">
+            <p className="font-semibold text-[2.4rem] leading-[3.2rem] text-black-400">견적서 목록</p>
+            <EstimationSortDropdown />
+            <div className="flex flex-col gap-[5.4rem]">
+              {EstimateData.map((item, index) => (
+                <div key={index}>
+                  <EstimateReceivedCard data={item} />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
