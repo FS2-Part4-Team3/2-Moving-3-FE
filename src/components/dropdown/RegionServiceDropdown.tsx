6db61abe2b82f04f941b79e078c27018b9ff1535
hyobiin9 <moone1234@kakao.com>
2025-01-14 23:45:22 +0900

    fix: #111-reset logic

--- src/components/dropdown/RegionServiceDropdown.tsx
+++ src/components/dropdown/RegionServiceDropdown.tsx
@@ -41,6 +41,8 @@ export default function RegionServiceDropdown() {
 
   const handleResetClick = () => {
     dispatch(resetSelection());
+    setSelectedRegion(null);
+    setSelectedService(null);
   };
 
   const handleRegionClick = (region: string) => {
