85ca514872aefeab1e5b84c522a5bfc66fb3ae75
unknown <d653@naver.com>
2025-01-14 15:34:47 +0900

    Feat:#115 - Set router

--- src/components/Tabs/ReviewTabs.tsx
+++ src/components/Tabs/ReviewTabs.tsx
@@ -1,15 +1,35 @@
 'use client';
 
-import { useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function ReviewTabs() {
-  const [isSelected, setIsSelected] = useState(false);
+  const pathName = usePathname();
+  const [isSelected, setIsSelected] = useState<'able' | 'my'>('able');
+
+  useEffect(() => {
+    if (pathName === '/normal/my-page/writable-review') {
+      setIsSelected('able');
+    }
+    if (pathName === '/normal/my-page/written-review') {
+      setIsSelected('my');
+    }
+  }, [pathName]);
 
   return (
     <div>
       <div className="flex items-center lg:gap-[4rem] bg-white shadow-custom7 lg:px-[26rem] lg:py-[3.2rem] ">
-        <div className={`text-[2.4rem] font-semibold cursor-pointer `}>작성 가능한 리뷰</div>
-        <div className={`text-[2.4rem] font-semibold cursor-pointer `}>내가 작성한 리뷰</div>
+        <Link href="/normal/my-page/writable-review">
+          <div className={`text-[2.4rem] font-semibold cursor-pointer ${isSelected ? 'text-gray-400' : 'text-black-400'} `}>
+            작성 가능한 리뷰
+          </div>
+        </Link>
+        <Link href="/normal/my-page/written-review">
+          <div className={`text-[2.4rem] font-semibold cursor-pointer ${isSelected ? 'text-gray-400' : 'text-black-400'} `}>
+            내가 작성한 리뷰
+          </div>
+        </Link>
       </div>
     </div>
   );
