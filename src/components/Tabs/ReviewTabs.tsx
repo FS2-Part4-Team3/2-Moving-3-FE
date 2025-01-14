9b7f60c154f6806fd1ba62bbaf1e26fedec885a1
unknown <d653@naver.com>
2025-01-14 16:29:03 +0900

    Feat:#115 - responsive design

--- src/components/Tabs/ReviewTabs.tsx
+++ src/components/Tabs/ReviewTabs.tsx
@@ -19,15 +19,21 @@ export default function ReviewTabs() {
 
   return (
     <div>
-      <div className="flex items-center lg:gap-[4rem] bg-white shadow-custom7 lg:px-[26rem] lg:py-[3.2rem] ">
+      <div className="lg:w-full md:w-[74.4rem] sm:W-[37.5rem] lg:h-[9.6rem] md:h-[5.4rem] sm:h-[5.4rem] flex items-center lg:gap-[4rem] md:gap-[2.4rem] sm:gap-[2.4remh] bg-white shadow-custom7 lg:px-[26rem] lg:py-[3.2rem] md:px-[7.2rem] md:py-[1rem] sm:px-[2.4rem] sm:py-[1rem] ">
         <Link href="/normal/my-page/writable-review">
-          <div className={`text-[2.4rem] font-semibold cursor-pointer ${isSelected ? 'text-gray-400' : 'text-black-400'} `}>
-            작성 가능한 리뷰
+          <div
+            className={`lg:h-[9.6rem] flex items-center cursor-pointer ${isSelected === 'able' ? 'text-black-400 border-b-[0.4rem] border-blue-400 ' : 'text-gray-400'} `}
+            onClick={() => setIsSelected('able')}
+          >
+            <span className="lg:text-[2.4rem] md:text-[1.4rem] sm:text-[1.4rem] font-semibold ">작성 가능한 리뷰</span>
           </div>
         </Link>
         <Link href="/normal/my-page/written-review">
-          <div className={`text-[2.4rem] font-semibold cursor-pointer ${isSelected ? 'text-gray-400' : 'text-black-400'} `}>
-            내가 작성한 리뷰
+          <div
+            className={`lg:h-[9.6rem] flex items-center cursor-pointer ${isSelected === 'my' ? 'text-black-400 border-b-[0.4rem] border-blue-400' : 'text-gray-400'} `}
+            onClick={() => setIsSelected('my')}
+          >
+            <span className="lg:text-[2.4rem] md:text-[1.4rem] sm:text-[1.4rem] font-semibold ">내가 작성한 리뷰</span>
           </div>
         </Link>
       </div>
