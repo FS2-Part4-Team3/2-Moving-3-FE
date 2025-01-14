a442e9c7c1f60a1c67e3c730da0637f4ef3f8846
hyobiin9 <moone1234@kakao.com>
2025-01-14 23:44:18 +0900

    feat: add api match driver client data

--- src/pages/MatchDriverClient.tsx
+++ src/pages/MatchDriverClient.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { useSelector } from 'react-redux';
 import { getDriverListData } from '@/api/DriverService';
 import FindDriverCard from '@/components/cards/FindDriverCard';
 import type { DriverListResponse } from '@/interfaces/API/DriverServiceInterface';
+import type { RootState } from '@/store/store';
 
 export default function MatchDriverClient() {
+  const { page, pageSize, keyword, orderBy, area, serviceType } = useSelector((state: RootState) => state.drivers);
+
   const {
     data: drivers,
     isLoading: driversLoading,
@@ -14,16 +18,16 @@ export default function MatchDriverClient() {
     hasNextPage,
     isFetchingNextPage,
   } = useInfiniteQuery<DriverListResponse>({
-    queryKey: ['drivers'],
+    queryKey: ['drivers', keyword, orderBy, area, serviceType],
     queryFn: ({ pageParam }) => {
-      return getDriverListData(pageParam as number);
+      return getDriverListData(pageParam as number, pageSize, keyword, orderBy, area, serviceType);
     },
     getNextPageParam: (lastPage, allPages) => {
       const currentPage = allPages.length;
       const totalPages = Math.ceil(lastPage.totalCount / 10);
       return currentPage < totalPages ? currentPage + 1 : undefined;
     },
-    initialPageParam: 1,
+    initialPageParam: page,
   });
 
   if (driversLoading) {
