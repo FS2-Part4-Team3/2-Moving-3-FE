48f5da6fb82f45e4d122de8e2a73c28c13a1a3dc
hyobiin9 <moone1234@kakao.com>
2025-01-15 17:41:46 +0900

    feat:#109-signup api connect logic

--- src/api/UserService.ts
+++ src/api/UserService.ts
@@ -13,3 +13,19 @@ export const postSignInData = async (userType: string, email: string, password: 
     throw error;
   }
 };
+
+export const postSignUpData = async (userType: string, email: string, name: string, phoneNumber: string, password: string) => {
+  const params = {
+    email: email,
+    name: name,
+    phoneNumber: phoneNumber,
+    password: password,
+  };
+  try {
+    const data = await postRequest(`/auth/signUp/${userType}`, params);
+    return data;
+  } catch (error) {
+    console.error('SignUp Fetch error:', error);
+    throw error;
+  }
+};
