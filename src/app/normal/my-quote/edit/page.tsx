0c32781c1c63cf9896416512b930d3aeac54d72a
hyobiin9 <moone1234@kakao.com>
2025-01-15 21:02:54 +0900

    feat: my quote edit page media query

--- src/app/normal/my-quote/edit/page.tsx
+++ src/app/normal/my-quote/edit/page.tsx
@@ -6,11 +6,13 @@ export default async function MyQuoteEdit() {
   const moveInfoData = await getEstimationData();
   console.log(moveInfoData[0]);
   return (
-    <div className="w-full min-h-screen bg-background-100 flex justify-center pt-[6.4rem]">
-      <div className="w-[120rem] h-fit bg-white rounded-[4rem] border border-line-100 py-[4.8rem] px-[4rem] flex flex-col gap-[4.8rem] shadow-custom3">
+    <div className="w-full min-h-screen bg-background-100 flex justify-center lg:pt-[6.4rem] md:pt-[5.4rem] sm:pt-[2.4rem]">
+      <div className="lg:w-[120rem] sm:w-full md:mx-[7.2rem] h-fit bg-white lg:rounded-[4rem] md:rounded-[2.4rem] border border-line-100 lg:py-[4.8rem] lg:px-[4rem] sm:py-[1.6rem] md:px-[3.2rem] sm:px-[2.4rem] flex flex-col lg:gap-[4.8rem] sm:gap-[3.2rem] md:shadow-custom3">
         <EstimationInformationCard data={moveInfoData[0]} />
-        <div className="flex flex-col gap-[4rem]">
-          <p className="font-semibold text-[2.4rem] leading-[3.2rem]">견적서 수정하기</p>
+        <div className="flex flex-col lg:gap-[4rem] sm:gap-[2.4rem]">
+          <p className="font-semibold lg:text-[2.4rem] lg:leading-[3.2rem] sm:text-[1.6rem] sm:leading-[2.6rem] text-black-400">
+            견적서 수정하기
+          </p>
           <MyQuoteEditClient />
         </div>
       </div>
--- src/pages/MyQuoteEditClient.tsx
+++ src/pages/MyQuoteEditClient.tsx
@@ -4,14 +4,14 @@ import { ButtonWrapper } from '@/components/common/headless/Button';
 
 export default function MyQuoteEditClient() {
   return (
-    <div className="w-full flex items-center lg:gap-[1.1rem]">
+    <div className="w-full flex items-center lg:gap-[1.1rem] sm:gap-[1.6rem] md:flex-row sm:flex-col">
       <ButtonWrapper id="edit-quotation">
-        <ButtonWrapper.Button className="w-full h-[6.4rem] rounded-[1.6rem] p-[1.6rem] font-semibold text-[2rem] leading-[3.2rem] text-white">
+        <ButtonWrapper.Button className="w-full lg:h-[6.4rem] sm:h-[4.8rem] lg:rounded-[1.6rem] sm:rounded-[0.8rem] p-[1.6rem] flex items-center justify-center font-semibold lg:text-[2rem] lg:leading-[3.2rem] sm:text-[1.6rem] sm:leading-[2.6rem] text-white">
           견적서 수정하기
         </ButtonWrapper.Button>
       </ButtonWrapper>
       <ButtonWrapper id="delete-quotation">
-        <ButtonWrapper.Button className="w-full h-[6.4rem] rounded-[1.6rem] py-[1.6rem] px-[2.4rem] border border-blue-300 bg-white font-semibold text-[2rem] leading-[3.2rem] text-blue-300">
+        <ButtonWrapper.Button className="w-full lg:h-[6.4rem] sm:h-[4.8rem] lg:rounded-[1.6rem] sm:rounded-[0.8rem] py-[1.6rem] px-[2.4rem] flex items-center justify-center border border-blue-300 bg-white font-semibold lg:text-[2rem] lg:leading-[3.2rem] sm:text-[1.6rem] sm:leading-[2.6rem] text-blue-300">
           견적서 삭제하기
         </ButtonWrapper.Button>
       </ButtonWrapper>
