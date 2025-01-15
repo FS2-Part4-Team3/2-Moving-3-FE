3a737b5f9aaf73ce66c110e187a887428ef024a7
hyobiin9 <moone1234@kakao.com>
2025-01-15 23:24:39 +0900

    feat:#130-delete modal

--- src/pages/MyQuoteEditClient.tsx
+++ src/pages/MyQuoteEditClient.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import Link from 'next/link';
 import { useState } from 'react';
 import { ButtonWrapper } from '@/components/common/headless/Button';
+import { ModalWrapper } from '@/components/common/headless/Modal';
 import MyQuoteEditToast from '@/components/toasts/MyQuoteEditToast';
 
 export default function MyQuoteEditClient() {
   //TODO: 기사님 정보가 있는 경우 없는 경우 나눠서 토스트 보여줄지 확정하기 라우팅 시켜줄지 결정하기
   const [showToast, setShowToast] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const handleShowToast = () => {
     setShowToast(true);
@@ -16,24 +19,47 @@ export default function MyQuoteEditClient() {
     setShowToast(false);
   };
 
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="w-full flex items-center lg:gap-[1.1rem] sm:gap-[1.6rem] md:flex-row sm:flex-col">
         <ButtonWrapper id="edit-quotation">
           <ButtonWrapper.Button
             className="w-full lg:h-[6.4rem] sm:h-[4.8rem] lg:rounded-[1.6rem] sm:rounded-[0.8rem] p-[1.6rem] flex items-center justify-center font-semibold lg:text-[2rem] lg:leading-[3.2rem] sm:text-[1.6rem] sm:leading-[2.6rem] text-white"
-            onClick={handleShowToast}
+            // onClick={handleShowToast}
           >
-            견적서 수정하기
+            <Link href="/normal/request-quote">견적서 수정하기</Link>
           </ButtonWrapper.Button>
         </ButtonWrapper>
-        <ButtonWrapper id="delete-quotation">
+        <ButtonWrapper id="delete-quotation" onClick={handleShowModal}>
           <ButtonWrapper.Button className="w-full lg:h-[6.4rem] sm:h-[4.8rem] lg:rounded-[1.6rem] sm:rounded-[0.8rem] py-[1.6rem] px-[2.4rem] flex items-center justify-center border border-blue-300 bg-white font-semibold lg:text-[2rem] lg:leading-[3.2rem] sm:text-[1.6rem] sm:leading-[2.6rem] text-blue-300">
             견적서 삭제하기
           </ButtonWrapper.Button>
         </ButtonWrapper>
       </div>
-      {showToast && <MyQuoteEditToast onClose={handleCloseToast} />}
+      {/* {showToast && <MyQuoteEditToast onClose={handleCloseToast} />} */}
+      {showModal && (
+        <div>
+          <div>
+            <ModalWrapper onClose={handleCloseModal}>
+              <ModalWrapper.Header>견적 삭제하기</ModalWrapper.Header>
+              <ModalWrapper.Content>
+                <p className="font-semibold text-[2.4rem] leading-[3.2rem] text-black-300 flex items-center justify-center">
+                  정말 삭제하시겠습니까?
+                </p>
+              </ModalWrapper.Content>
+              <ModalWrapper.Footer isDisabled={false}>견적 삭제하기</ModalWrapper.Footer>
+            </ModalWrapper>
+          </div>
+        </div>
+      )}
     </>
   );
 }
