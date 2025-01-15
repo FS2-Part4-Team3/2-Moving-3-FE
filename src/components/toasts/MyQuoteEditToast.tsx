526ee6fbd0e4ae267b224ed888a35f90021fafcd
hyobiin9 <moone1234@kakao.com>
2025-01-15 21:50:05 +0900

    feat:#130-my quote edit toast media query

--- src/components/toasts/MyQuoteEditToast.tsx
+++ src/components/toasts/MyQuoteEditToast.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import { useEffect } from 'react';
 import info from '@/../public/assets/toast/ic_red_info.svg';
@@ -13,9 +15,12 @@ export default function MyQuoteEditToast({ onClose }: MyQuoteEditToastProps) {
   }, [onClose]);
 
   return (
-    <div>
+    <div className="lg:w-[120rem] md:w-[60rem] sm:w-[32.7rem] rounded-[1.2rem] border border-red-200 lg:gap-[1.6rem] sm:gap-[0.8rem] lg:py-[2.4rem] lg:px-[3.2rem] sm:py-[1rem] sm:px-[2.4rem] flex items-center bg-red-100">
       <Image src={info} alt="info" width={24} height={24} />
-      <p>견적을 보낸 기사님이 있어서 견적서를 수정할 수 없어요!</p>
+      <p className="font-semibold lg:text-[1.6rem] lg:leading-[2.6rem] sm:text-[1.3rem] sm:leading-[2.2rem] text-red-200 sm:text-center md:text-start">
+        견적을 보낸 기사님이 있어서
+        <br className="md:hidden sm:block" /> 견적서를 수정할 수 없어요!
+      </p>
     </div>
   );
 }
