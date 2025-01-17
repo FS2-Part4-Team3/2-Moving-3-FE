8064f343c218a8ecda0626893aec39d2f74c8c62
hyobiin9 <moone1234@kakao.com>
2025-01-17 23:54:58 +0900

    fix:#156-routing setting

--- src/app/callback/google/page.tsx
+++ src/app/callback/google/page.tsx
@@ -36,9 +36,16 @@ export default function CallBackGoogle() {
           type: res.type,
         }),
       );
-      if (res.type === 'user') {
+      if (res.type === 'user' && (!res.image || !res.areas || !res.serviceTypes)) {
+        router.push('/normal/profile-register');
+      } else if (res.type === 'user' && res.image && res.areas && res.serviceTypes) {
         router.push('/normal/match-driver');
-      } else if (res.type === 'driver') {
+      } else if (
+        res.type === 'driver' &&
+        (!res.image || (!res.introduce && !res.description && !res.availableAreas && !res.nickname))
+      ) {
+        router.push('/driver/profile-register');
+      } else if (res.type === 'driver' && res.image && res.introduce && res.description && res.availableAreas) {
         router.push('/driver/receive-quote');
       }
     };
