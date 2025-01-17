ef3a331ea66ebb657b1d0a2552e1b98a426e0ad2
hyobiin9 <moone1234@kakao.com>
2025-01-18 00:30:58 +0900

    feat:#158-refresh token issuance

--- src/api/api.ts
+++ src/api/api.ts
@@ -16,7 +16,7 @@ export async function fetchWrapper(url: string, options: RequestInit = {}) {
     ...options.headers,
   };
 
-  const urlWithCacheBusting = `${BASE_URL}${url}?t=${new Date().getTime()}`;
+  // const urlWithCacheBusting = `${BASE_URL}${url}?t=${new Date().getTime()}`;
 
   try {
     const response = await fetch(`${BASE_URL}${url}`, {
@@ -25,6 +25,39 @@ export async function fetchWrapper(url: string, options: RequestInit = {}) {
     });
 
     if (!response.ok) {
+      if (response.status === 401) {
+        const refreshToken = getCookie('refreshToken');
+        const refreshResponse = await fetch(`${BASE_URL}/auth/refresh`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ refreshToken }),
+        });
+
+        if (!refreshResponse.ok) {
+          throw new CustomError('Failed to refresh Token', await refreshResponse.json());
+        }
+
+        const { accessToken } = await refreshResponse.json();
+        localStorage.setItem('accessToken', accessToken);
+
+        const retryResponse = await fetch(`${BASE_URL}${url}`, {
+          ...options,
+          headers: {
+            ...headers,
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        if (!retryResponse.ok) {
+          const error = new CustomError(`HTTP error! status: ${retryResponse.status}`, await retryResponse.json());
+          throw error;
+        }
+
+        return retryResponse.json();
+      }
+
       console.error('Response not OK:', {
         status: response.status,
         statusText: response.statusText,
@@ -39,4 +72,10 @@ export async function fetchWrapper(url: string, options: RequestInit = {}) {
     console.error('Fetch error:', error);
     throw error;
   }
+}
+
+function getCookie(name: string) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
 }
