943fa5691d998210057a0108c434e2c0c5a7d6d8
hyobiin9 <moone1234@kakao.com>
2025-01-20 18:31:31 +0900

    feat:#164-add dibs driver page card storybook

--- src/components/cards/DibsDriverPageCard.tsx
+++ src/components/cards/DibsDriverPageCard.tsx
@@ -2,37 +2,40 @@ import Image from 'next/image';
 import emptyHeart from '@/../public/assets/driver/ic_empty_like.svg';
 import redHeart from '@/../public/assets/driver/ic_like_on.svg';
 import star from '@/../public/assets/driver/ic_star_yellow.svg';
+import { DibsDriverPageCardProps } from '@/interfaces/Card/DibsDriverPageCardInterface';
 import MovingTypeChips from '../chips/MovingTypeChips';
 
-export default function DibsDriverPageCard() {
+export default function DibsDriverPageCard({ data }: DibsDriverPageCardProps) {
   return (
     <div>
       <div>
-        <MovingTypeChips type="SMALL" />
+        {data.serviceType.map((item: string) => (
+          <MovingTypeChips type={item as 'SMALL' | 'HOME' | 'OFFICE' | 'APPOINTMENT' | 'WAITING' | 'RECEIVED'} />
+        ))}
         <div>
-          {/*Driver Image*/}
+          {data.image && <Image src={data.image} alt="profile" width={80} height={80} />}
           <div>
             <div>
-              <p>김코드 기사님</p>
+              <p>{data.name} 기사님</p>
               <div>
                 <Image src={emptyHeart} alt="heart" width={24} height={24} />
-                <div>123</div>
+                <div>{data.likeCount}</div>
               </div>
             </div>
             <div>
               <div>
                 <Image src={star} alt="star" width={24} height={24} />
-                <p>5.0</p>
-                <p>(178)</p>
+                <p>{data.rating}</p>
+                <p>({data.reviewCount})</p>
               </div>
               <div />
               <div>
                 <p>경력</p>
-                <p>7년</p>
+                <p>{data.career}년</p>
               </div>
               <div />
               <div>
-                <p>334건</p>
+                <p>{data.applyCount}건</p>
                 <p>확정</p>
               </div>
             </div>
