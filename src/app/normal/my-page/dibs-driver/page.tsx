8380ce6ec48915c2b5cc67663b02aa19b7880747
hyobiin9 <moone1234@kakao.com>
2025-01-20 15:01:54 +0900

    feat:#164-dibs driver page ui initial structure

---
+++ src/app/normal/my-page/dibs-driver/page.tsx
@@ -0,0 +1,10 @@
+export default function DibsDriverPage() {
+  return (
+    <div className="w-full bg-background-100 min-h-screen">
+      <div className="w-full bg-white py-[3.2rem] gap-[1rem] box-custom7 flex items-center justify-center">
+        <p className="w-[120rem] font-semibold text-[2.4rem] leading-[3.2rem]">찜한 기사님</p>
+      </div>
+      <div className="w-full flex items-center justify-center"></div>
+    </div>
+  );
+}
