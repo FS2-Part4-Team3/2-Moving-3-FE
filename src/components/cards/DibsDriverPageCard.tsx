56d6ad0dfd920c2d3ca6dec4b2bfe6ec8a19ea6f
hyobiin9 <moone1234@kakao.com>
2025-01-20 18:44:43 +0900

    style:#164-dibs driver page card pc ui

--- src/components/cards/DibsDriverPageCard.tsx
+++ src/components/cards/DibsDriverPageCard.tsx
@@ -7,37 +7,41 @@ import MovingTypeChips from '../chips/MovingTypeChips';
 
 export default function DibsDriverPageCard({ data }: DibsDriverPageCardProps) {
   return (
-    <div>
-      <div>
+    <div className="w-full rounded-[1.6rem] border py-[2rem] px-[2.4rem] border-line-100 shadow-custom3 flex flex-col gap-[1.6rem]">
+      <div className="w-full flex gap-[1.2rem]">
         {data.serviceType.map((item: string) => (
           <MovingTypeChips type={item as 'SMALL' | 'HOME' | 'OFFICE' | 'APPOINTMENT' | 'WAITING' | 'RECEIVED'} />
         ))}
+      </div>
+      <div className="w-full rounded-[0.6rem] border border-line-100 py-[1.6rem] px-[1.8rem] flex gap-[2.4rem] shadow-custom5">
         <div>
-          {data.image && <Image src={data.image} alt="profile" width={80} height={80} />}
-          <div>
-            <div>
-              <p>{data.name} 기사님</p>
-              <div>
-                <Image src={emptyHeart} alt="heart" width={24} height={24} />
-                <div>{data.likeCount}</div>
-              </div>
+          {data.image && (
+            <Image src={data.image} alt="profile" width={80} height={80} className="border-2 border-blue-400 rounded-full" />
+          )}
+        </div>
+        <div className="w-full flex flex-col gap-[1.6rem]">
+          <div className="w-full flex items-center justify-between">
+            <p className="font-semibold text-[1.8rem] leading-[2.6rem] text-black-300">{data.name} 기사님</p>
+            <div className="flex gap-[0.4rem] items-center">
+              <Image src={emptyHeart} alt="heart" width={24} height={24} />
+              <p className="font-medium text-[1.8rem] leading-[2.6rem]">{data.likeCount}</p>
             </div>
-            <div>
-              <div>
-                <Image src={star} alt="star" width={24} height={24} />
-                <p>{data.rating}</p>
-                <p>({data.reviewCount})</p>
-              </div>
-              <div />
-              <div>
-                <p>경력</p>
-                <p>{data.career}년</p>
-              </div>
-              <div />
-              <div>
-                <p>{data.applyCount}건</p>
-                <p>확정</p>
-              </div>
+          </div>
+          <div className="w-full flex gap-[1.6rem] items-center">
+            <div className="flex gap-[0.6rem] items-center">
+              <Image src={star} alt="star" width={24} height={24} />
+              <p className="font-medium text-[1.6rem] leading-[2.6rem] text-black-300">{data.rating}</p>
+              <p className="font-medium text-[1.6rem] leading-[2.6rem] text-gray-300">({data.reviewCount})</p>
+            </div>
+            <div className="border border-line-200 h-[1.4rem]" />
+            <div className="flex gap-[0.6rem]">
+              <p className="font-medium text-[1.6rem] leading-[2.6rem] text-gray-300">경력</p>
+              <p className="font-medium text-[1.6rem] leading-[2.6rem] text-black-300">{data.career}년</p>
+            </div>
+            <div className="border border-line-200 h-[1.4rem]" />
+            <div className="flex gap-[0.6rem]">
+              <p className="font-medium text-[1.6rem] leading-[2.6rem] text-black-300">{data.applyCount}건</p>
+              <p className="font-medium text-[1.6rem] leading-[2.6rem] text-gray-300">확정</p>
             </div>
           </div>
         </div>
