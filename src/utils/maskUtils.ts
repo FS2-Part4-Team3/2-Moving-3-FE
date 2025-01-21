f8ee7677bb3e60ff184f7b539d4366a62d7b338d
Jaewon Kim <magry78@gmail.com>
2025-01-21 13:16:58 +0900

    feat:#151-maskUtils jest test

--- src/utils/maskUtils.ts
+++ src/utils/maskUtils.ts
@@ -1,4 +1,12 @@
-export function maskName(name: string) {
+export function maskName(name: string): string {
+  // 이름에 공백이 포함되어 있으면, 공백을 그대로 두고 나머지 부분 각각 마스킹
+  if (name.includes(' ')) {
+    return name
+      .split(' ')
+      .map((part: string) => maskName(part))
+      .join(' ');
+  }
+
   if (name.length === 2) {
     // 이름이 두 글자일 경우 끝 글자 마스킹
     return `${name[0]}*`;
