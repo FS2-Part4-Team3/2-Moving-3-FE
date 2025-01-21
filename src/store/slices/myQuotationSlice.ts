a57fbabb010bacce8d651181505dfc94c56e2700
unknown <d653@naver.com>
2025-01-21 15:13:56 +0900

    Feat:#180 - create myQuotation slice

---
+++ src/store/slices/myQuotationSlice.ts
@@ -0,0 +1,22 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+interface MyQuotation {
+  id: string;
+}
+
+const initialState: MyQuotation = {
+  id: '',
+};
+
+const myQuotationSlice = createSlice({
+  name: 'myQuotation',
+  initialState,
+  reducers: {
+    setId(state, action: PayloadAction<string>) {
+      state.id = action.payload;
+    },
+  },
+});
+
+export const { setId } = myQuotationSlice.actions;
+export default myQuotationSlice.reducer;
