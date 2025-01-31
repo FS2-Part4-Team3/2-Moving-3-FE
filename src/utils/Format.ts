a79b8eaa1f1f3c2fc79646f6081070567ab48025
unknown <d653@naver.com>
2025-01-31 16:29:39 +0900

    Fix:#241 - add showing time of formatDate

--- src/utils/Format.ts
+++ src/utils/Format.ts
@@ -82,7 +82,12 @@ export function priceFormat(price: number) {
 }
 
 export function formatDate(date: Date) {
-  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const period = hours < 12 ? '오전' : '오후';
+  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+
+  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 ${period} ${formattedHours}:${String(minutes).padStart(2, '0')} `;
 }
 
 export function getDaysInMonth(date: Date): Day[] {
