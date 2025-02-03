1601ebf51898fb6f2d915d6941c92e8cc1306c53
Jaewon Kim <magry78@gmail.com>
2025-02-03 13:41:15 +0900

    feat:#245-viewport setting

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    viewportWidth: 1400,
+    viewportHeight: 900,
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {},
     supportFile: false,
