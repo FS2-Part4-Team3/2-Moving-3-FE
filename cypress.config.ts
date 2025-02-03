02ebedaff86735a7de6fa38a6fa36bdd8d51dae1
Jaewon Kim <magry78@gmail.com>
2025-02-03 17:50:50 +0900

    feat:#245-hydration error exception

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    viewportWidth: 1400,
+    viewportWidth: 1200,
     viewportHeight: 900,
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {},
-    supportFile: false,
-    // supportFile: 'cypress/support/e2e.ts',
+    supportFile: 'cypress/support/e2e.ts',
   },
 });
