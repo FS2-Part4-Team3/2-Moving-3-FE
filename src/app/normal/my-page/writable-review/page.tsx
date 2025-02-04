ddf566e92f42081e0a843d174e02ac03136f061c
unknown <d653@naver.com>
2025-02-04 16:37:19 +0900

    Feat:#149 - add WritableReviewClient

--- src/app/normal/my-page/writable-review/page.tsx
+++ src/app/normal/my-page/writable-review/page.tsx
@@ -1,7 +1,6 @@
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
 import { getReviewableEstimations } from '@/api/EstimationService';
 import ReviewTabs from '@/components/Tabs/ReviewTabs';
-import NormalReviewCard from '@/components/cards/NormalReviewCard';
 
 export default async function WritableReviewPage() {
   const queryClient = new QueryClient();
@@ -19,11 +18,7 @@ export default async function WritableReviewPage() {
         <div className="w-full flex justify-center">
           <ReviewTabs />
         </div>
-        <div className="flex flex-col items-center gap-[4rem] bg-background-100 lg:pt-[4rem]">
-          <div className="lg:grid lg:grid-cols-2 lg:gap-y-12 lg:gap-x-6 md:flex md:flex-col sm:flex sm:flex-col md:gap-y-8 sm:gap-y-8">
-            {/* <NormalReviewCard type="ABLE" /> */}
-          </div>
-        </div>
+        <WritableReviewPage />
       </>
     </HydrationBoundary>
   );
