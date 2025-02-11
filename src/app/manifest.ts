45da4f7c9ca03fb64e9c4559c448022f1c7ca340
hyobiin9 <moone1234@kakao.com>
2025-02-11 10:27:33 +0900

    feat:#301-add color option

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,6 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: '이사 소비자와 이사 전문가 매칭 서비스',
     start_url: '/',
     display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#ffffff',
     icons: [
       {
         src: '/favicon.ico',
