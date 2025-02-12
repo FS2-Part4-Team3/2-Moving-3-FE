30f8f40b07933989c6a4c8ca82467ec3c09700df
Jaewon Kim <magry78@gmail.com>
2025-02-12 16:01:29 +0900

    fix:#314-width 1200px fit

--- src/app/driver/my-quote/sent/[id]/page.tsx
+++ src/app/driver/my-quote/sent/[id]/page.tsx
@@ -5,7 +5,7 @@ export default async function MyQuoteSentDetail({ params }: { params: { id: stri
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="bg-white lg:w-[129.6rem] md:w-[60rem] sm:w-[32.7rem]">
+      <div className="bg-white lg:w-[120rem] md:w-[60rem] sm:w-[32.7rem]">
         <p className="lg:text-[2.4rem] lg:leading-[3.2rem] sm:text-[1.8rem] sm:leading-[2.6rem] font-semibold text-black-400 lg:py-[3.2rem] sm:py-[1.4rem]">
           견적 상세
         </p>
