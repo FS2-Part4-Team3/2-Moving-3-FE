b90e6886120d69efae7d1b5e6407cb0a387317c7
hyobiin9 <moone1234@kakao.com>
2025-02-14 23:52:54 +0900

    feat:#337-add reaction

--- src/components/cards/ReviewSummaryCard.tsx
+++ src/components/cards/ReviewSummaryCard.tsx
@@ -21,16 +21,18 @@ export default function ReviewSummaryCard({ driverId }: ReviewSummaryCardProps) 
   }
 
   return (
-    <div className="bg-background-200 rounded-[3.2rem] flex flex-col gap-[0.8rem] py-[2rem] px-[3rem]">
-      <div className="flex gap-[3rem]">
-        <p className="font-semibold text-[2rem] leading-[3.2rem] text-gray-400">리뷰 요약</p>
-        <p
-          className={`leading-[3.2rem] ${reviewSummaryData?.summaryReview ? 'font-semibold text-[2rem] text-black-400' : 'text-medium text-[2rem] text-red-200'}`}
-        >
+    <div className="bg-background-200 rounded-[3.2rem] flex flex-col gap-[1.6rem] py-[2rem] px-[3rem]">
+      <div className="flex flex-col gap-[0.4rem]">
+        <p className="md:font-semibold md:text-[2rem] md:leading-[3.2rem] sm:font-bold sm:text-[1.6rem] sm:leading-[2.6rem] text-gray-400">
+          리뷰 요약
+        </p>
+        <p className="md:leading-[3.2rem] sm:leading-[2.6rem] md:font-semibold sm:font-bold md:text-[2rem] sm:text-[1.6rem] text-black-400">
           {reviewSummaryData?.summaryReview || '생성된 리뷰가 존재하지 않습니다.'}
         </p>
       </div>
-      <p className="font-semibold text-[1.8rem] text-red-200">*해당 정보는 매일 자정에 갱신됩니다.</p>
+      <p className="md:font-semibold md:text-[1.8rem] sm:font-bold sm:text-[1.6rem] text-red-200">
+        *해당 정보는 매일 자정에 갱신됩니다.
+      </p>
     </div>
   );
 }
