43e7a7f4732a3795bd952e39ff5420021d0d3967
hyobiin9 <moone1234@kakao.com>
2025-02-16 21:21:52 +0900

    feat:#295-add accesstoken logic in callback pages

--- src/api/UserService.ts
+++ src/api/UserService.ts
@@ -147,3 +147,13 @@ export const deleteRefresh = async () => {
     throw error;
   }
 };
+
+export const getAuthIsLoggedIn = async () => {
+  try {
+    const res = await getRequest('/auth/isLoggedIn');
+    return res;
+  } catch (error) {
+    console.error('Get Auth Login Fetch Error', error);
+    throw error;
+  }
+};
--- src/app/callback/naver/page.tsx
+++ src/app/callback/naver/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getUserData } from '@/api/UserService';
+import { getAuthIsLoggedIn, getUserData } from '@/api/UserService';
 import { setUserSign } from '@/store/slices/SignInSlice';
 
 export default function CallBackNaver() {
@@ -11,36 +11,47 @@ export default function CallBackNaver() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getData = async () => {
-      const res = await getUserData();
-      dispatch(
-        setUserSign({
-          id: res.id,
-          name: res.name,
-          nickname: res.type === 'driver' ? res.nickname : undefined,
-          email: res.email,
-          image: res.image,
-          phoneNumber: res.phoneNumber,
-          introduce: res.introduce,
-          description: res.description,
-          serviceType: res.serviceType,
-          availableAreas: res.type === 'driver' ? res.availableAreas : undefined,
-          areas: res.type === 'user' ? res.areas : undefined,
-          type: res.type,
-        }),
-      );
-      if (res.type === 'user' && (!res.areas || !res.serviceType.length)) {
-        router.push('/normal/profile-register');
-      } else if (res.type === 'user' && res.areas && res.serviceType.length) {
-        router.push('/normal/match-driver');
-      } else if (res.type === 'driver' && !res.introduce && !res.description && !res.availableAreas.length && !res.nickname) {
-        router.push('/driver/profile-register');
-      } else if (res.type === 'driver' && res.introduce && res.description && res.availableAreas.length) {
-        router.push('/driver/receive-quote');
+    const checkLoginStatus = async () => {
+      try {
+        const loggedInUser = await getAuthIsLoggedIn();
+
+        if (loggedInUser.isAccessTokenValid) {
+          return;
+        }
+
+        const res = await getUserData();
+        dispatch(
+          setUserSign({
+            id: res.id,
+            name: res.name,
+            nickname: res.type === 'driver' ? res.nickname : undefined,
+            email: res.email,
+            image: res.image,
+            phoneNumber: res.phoneNumber,
+            introduce: res.introduce,
+            description: res.description,
+            serviceType: res.serviceType,
+            availableAreas: res.type === 'driver' ? res.availableAreas : undefined,
+            areas: res.type === 'user' ? res.areas : undefined,
+            type: res.type,
+          }),
+        );
+        if (res.type === 'user' && (!res.areas || !res.serviceTypes)) {
+          router.push('/normal/profile-register');
+        } else if (res.type === 'user' && res.areas && res.serviceTypes) {
+          router.push('/normal/match-driver');
+        } else if (res.type === 'driver' && !res.introduce && !res.description && !res.availableAreas && !res.nickname) {
+          router.push('/driver/profile-register');
+        } else if (res.type === 'driver' && res.introduce && res.description && res.availableAreas) {
+          router.push('/driver/receive-quote');
+        }
+      } catch (error) {
+        console.error('로그인 상태 확인 중 오류 발생', error);
+        return;
       }
     };
 
-    getData();
+    checkLoginStatus();
   }, [dispatch, router]);
 
   return (
