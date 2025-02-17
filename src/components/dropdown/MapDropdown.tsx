6fa6392828cf58ac4772cee0b383fd1fbed015d8
Jaewon Kim <magry78@gmail.com>
2025-02-17 16:39:22 +0900

    feat:#244-MyQuoteSentDetail page dark add

--- src/components/dropdown/MapDropdown.tsx
+++ src/components/dropdown/MapDropdown.tsx
@@ -14,16 +14,18 @@ export default function MapDropdown({ selectedMenu, setSelectedMenu }: MapDropdo
   return (
     <div className="w-[13rem]">
       <div
-        className="border border-line-200 py-[0.6rem] px-[1rem] rounded-[0.8rem] flex justify-between cursor-pointer"
+        className="border border-line-200 py-[0.6rem] px-[1rem] rounded-[0.8rem] flex justify-between cursor-pointer dark:bg-dark-p"
         onClick={() => setIsDropdownOpen(!isDropdownOpen)}
       >
-        <p className={`font-semibold text-[2.4rem] leading-[2.6rem] ${selectedMenu ? 'text-black' : 'text-gray-400'}`}>
+        <p
+          className={`font-semibold text-[2.4rem] leading-[2.6rem] ${selectedMenu ? 'text-black dark:text-dark-t' : 'text-gray-400'}`}
+        >
           {selectedMenu || '선택'}
         </p>
         <Image src={arrow} alt="arrow" width={20} height={20} />
       </div>
       {isDropdownOpen && (
-        <div className="absolute flex flex-col items-start justify-center w-[11.5rem] bg-white rounded-[0.8rem] border border-line-200 mt-[0.8rem]">
+        <div className="absolute flex flex-col items-start justify-center w-[11.5rem] bg-white dark:bg-dark-p rounded-[0.8rem] border border-line-200 mt-[0.8rem]">
           <p
             className="font-medium text-[1.8rem] leading-[2.4rem] py-[0.6rem] px-[1.4rem] cursor-pointer"
             onClick={() => handleMenuClick('출발지')}
