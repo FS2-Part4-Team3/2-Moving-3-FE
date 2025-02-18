62fb49949214cdda49a16bf3eb27095bee74f27a
Jaewon Kim <magry78@gmail.com>
2025-02-18 22:14:57 +0900

    feat:#244-delete transform css

--- src/components/common/gnb/ModeToggle.tsx
+++ src/components/common/gnb/ModeToggle.tsx
@@ -22,17 +22,13 @@ export function ModeToggle() {
   return (
     <button
       onClick={() => setTheme(isDark ? 'light' : 'dark')}
-      className={`lg:w-[7.5rem] lg:h-[4rem] sm:h-[2rem] sm:w-[4rem] rounded-[3rem] gap-[0.5rem] flex items-center transition-all border border-dark-t lg:pl-[0.3rem] lg:dark:pr-[0.3rem] lg:dark:pl-0 sm:pl-[0.2rem] sm:dark:pr-[0.2rem] sm:dark:pl-0 dark:justify-end
+      className={`lg:w-[7.5rem] lg:h-[4rem] sm:h-[2rem] sm:w-[4rem] rounded-[3rem] gap-[0.5rem] flex items-center border border-dark-t lg:pl-[0.3rem] lg:dark:pr-[0.3rem] lg:dark:pl-0 sm:pl-[0.2rem] sm:dark:pr-[0.2rem] sm:dark:pl-0 dark:justify-end
         ${isDark ? 'bg-dark-bg text-dark-t' : 'bg-gray-100 text-black-400'}
       `}
     >
-      <span
-        className={`text-[1rem] font-semibold transition-all ${isDark ? 'lg:block sm:hidden opacity-100' : 'opacity-0 hidden'}`}
-      >
-        NIGHT
-      </span>
+      <span className={`text-[1rem] font-semibold ${isDark ? 'lg:block sm:hidden opacity-100' : 'opacity-0 hidden'}`}>NIGHT</span>
       <div
-        className={`lg:w-[3rem] lg:h-[3rem] sm:w-[1.5rem] sm:h-[1.5rem] bg-white rounded-full flex items-center justify-center transition-transform`}
+        className={`lg:w-[3rem] lg:h-[3rem] sm:w-[1.5rem] sm:h-[1.5rem] bg-white rounded-full flex items-center justify-center`}
       >
         {isDark ? (
           <Image src={moon} alt="dark" width={20} height={20} className="lg:block sm:hidden" />
@@ -45,9 +41,7 @@ export function ModeToggle() {
           <Image src={sun} alt="light" width={10} height={10} className="lg:hidden sm:block" />
         )}
       </div>
-      <span className={`text-[1rem] font-bold transition-all ${isDark ? 'opacity-0 hidden' : 'opacity-100 lg:block sm:hidden'}`}>
-        DAY
-      </span>
+      <span className={`text-[1rem] font-bold ${isDark ? 'opacity-0 hidden' : 'opacity-100 lg:block sm:hidden'}`}>DAY</span>
     </button>
   );
 }
