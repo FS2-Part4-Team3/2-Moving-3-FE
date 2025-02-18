05eb174406c3b09efaed26f30516cc7f617198bb
unknown <d653@naver.com>
2025-02-18 09:32:27 +0900

    Fix:#344 - error of written reviews page

--- src/_pages/WrittenReviewClient.tsx
+++ src/_pages/WrittenReviewClient.tsx
@@ -70,13 +70,21 @@ export default function WrittenReviewClient() {
     alert('에러가 발생했습니다. 다시 한 번 시도해주세요!');
   }
 
+  console.log('myReviews', myReviews);
+
   return (
-    <div className="h-screen flex flex-col items-center gap-[4rem] bg-background-100 ">
+    <div className="h-screen flex flex-col items-center gap-[4rem] bg-background-100 pt-[4rem]">
       {myReviews?.list.length ? (
-        <div className="lg:grid lg:grid-cols-2 lg:gap-y-12 lg:gap-x-6 md:flex md:flex-col sm:flex sm:flex-col md:gap-y-8 sm:gap-y-8">
-          {myReviews?.list.map(myReview => <WrittenReviewCard myReview={myReview} />)}
+        <>
+          <div className="lg:grid lg:grid-cols-2 lg:gap-y-[4.8rem] lg:gap-x-[4rem] md:flex md:flex-col sm:flex sm:flex-col md:gap-y-[3.2rem] sm:gap-y-[3.2rem] ">
+            {myReviews?.list.map(myReview => (
+              <div key={myReview.id}>
+                <WrittenReviewCard myReview={myReview} />
+              </div>
+            ))}
+          </div>
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
-        </div>
+        </>
       ) : (
         <div className="lg:pt-[30rem] md:pt-[19.4rem] sm:pt-[19.4rem]">
           <Empty type="Review" />
