d646ede7d530daf34aeab13e85737a538b78464b
Jaewon Kim <magry78@gmail.com>
2025-02-18 22:00:39 +0900

    feat:#244-modetoggle Reactive Implementation

--- src/components/common/gnb/ModeToggle.tsx
+++ src/components/common/gnb/ModeToggle.tsx
@@ -22,21 +22,32 @@ export function ModeToggle() {
   return (
     <button
       onClick={() => setTheme(isDark ? 'light' : 'dark')}
-      className={`w-[7.5rem] h-[4rem] rounded-[3rem] gap-[0.5rem] flex items-center transition-all border border-dark-t pl-[0.3rem] dark:pr-[0.3rem] dark:pl-0 dark:justify-end
+      className={`lg:w-[7.5rem] lg:h-[4rem] sm:h-[2rem] sm:w-[4rem] rounded-[3rem] gap-[0.5rem] flex items-center transition-all border border-dark-t lg:pl-[0.3rem] lg:dark:pr-[0.3rem] lg:dark:pl-0 sm:pl-[0.2rem] sm:dark:pr-[0.2rem] sm:dark:pl-0 dark:justify-end
         ${isDark ? 'bg-dark-bg text-dark-t' : 'bg-gray-100 text-black-400'}
       `}
     >
-      <span className={`text-[1rem] font-semibold transition-all ${isDark ? 'block opacity-100' : 'opacity-0 hidden'}`}>
+      <span
+        className={`text-[1rem] font-semibold transition-all ${isDark ? 'lg:block sm:hidden opacity-100' : 'opacity-0 hidden'}`}
+      >
         NIGHT
       </span>
-      <div className={`w-[3rem] h-[3rem] bg-white rounded-full flex items-center justify-center transition-transform`}>
+      <div
+        className={`lg:w-[3rem] lg:h-[3rem] sm:w-[1.5rem] sm:h-[1.5rem] bg-white rounded-full flex items-center justify-center transition-transform`}
+      >
         {isDark ? (
           <Image src={moon} alt="dark" width={20} height={20} className="lg:block sm:hidden" />
         ) : (
           <Image src={sun} alt="light" width={20} height={20} className="lg:block sm:hidden" />
         )}
+        {isDark ? (
+          <Image src={moon} alt="dark" width={10} height={10} className="lg:hidden sm:block" />
+        ) : (
+          <Image src={sun} alt="light" width={10} height={10} className="lg:hidden sm:block" />
+        )}
       </div>
-      <span className={`text-[1rem] font-bold transition-all ${isDark ? 'opacity-0 hidden' : 'opacity-100 block'}`}>DAY</span>
+      <span className={`text-[1rem] font-bold transition-all ${isDark ? 'opacity-0 hidden' : 'opacity-100 lg:block sm:hidden'}`}>
+        DAY
+      </span>
     </button>
   );
 }
