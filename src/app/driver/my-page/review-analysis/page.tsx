6563a13adb2d7ecd98ec9b13b1d93b4e7582b810
hyobiin9 <moone1234@kakao.com>
2025-02-19 11:34:10 +0900

    feat:#351-add detail

--- src/app/driver/my-page/review-analysis/page.tsx
+++ src/app/driver/my-page/review-analysis/page.tsx
@@ -1,10 +1,49 @@
 import ReviewAnalysisChart from '@/components/cards/ReviewAnalysisChart';
 
+const mockData = {
+  positive: [
+    {
+      keyword: '무사히',
+      count: 1,
+    },
+    {
+      keyword: '세심한 서비스',
+      count: 1,
+    },
+    {
+      keyword: '능숙한',
+      count: 1,
+    },
+    {
+      keyword: '친절',
+      count: 3,
+    },
+  ],
+  negative: [
+    {
+      keyword: '속도',
+      count: 1,
+    },
+    {
+      keyword: '짐 손상',
+      count: 1,
+    },
+    {
+      keyword: '추가적인 정리',
+      count: 2,
+    },
+    {
+      keyword: '아쉬웠습니다',
+      count: 2,
+    },
+  ],
+};
+
 export default function ReviewAnalysis() {
   return (
     <div>
       <div>
-        <ReviewAnalysisChart />
+        <ReviewAnalysisChart data={mockData} />
       </div>
     </div>
   );
--- src/components/cards/ReviewAnalysisChart.tsx
+++ src/components/cards/ReviewAnalysisChart.tsx
@@ -6,7 +6,7 @@ import { Bar } from 'react-chartjs-2';
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 interface ReviewAnalysisChartProps {
-  type: 'ALL' | 'POSITIVE' | 'NEGATIVE';
+  // type: 'ALL' | 'POSITIVE' | 'NEGATIVE';
   data: {
     positive: {
       keyword: string;
@@ -19,36 +19,54 @@ interface ReviewAnalysisChartProps {
   };
 }
 
-export default function ReviewAnalysisChart({ type, data }: ReviewAnalysisChartProps) {
+export default function ReviewAnalysisChart({ data }: ReviewAnalysisChartProps) {
   let labels: string[] = [];
   let counts: number[] = [];
   let colors: string[] = [];
+
+  let positiveLabels: string[] = [];
+  let negativeLabels: string[] = [];
+
+  let positiveCounts: number[] = [];
+  let negativeCounts: number[] = [];
+
+  let type = 'ALL';
 
   if (type === 'ALL') {
     // TODO: 백엔드 측에서 수정되면 slice 로직 삭제하기
     const positiveData = data.positive.slice(0, 5);
     const negativeData = data.negative.slice(0, 5);
 
-    labels = [...positiveData.map(item => item.keyword), ...negativeData.map(item => item.keyword)];
-    counts = [...positiveData.map(item => item.count), ...negativeData.map(item => item.count)];
-    colors = [...Array(positiveData.length).fill('#1B92FF'), ...Array(negativeData.length).fill('#FF4D4D')];
+    positiveLabels = positiveData.map(item => item.keyword);
+    positiveCounts = positiveData.map(item => item.count);
+
+    negativeLabels = negativeData.map(item => item.keyword);
+    negativeCounts = negativeData.map(item => item.count);
   } else if (type === 'POSITIVE') {
-    labels = data.positive.map(item => item.keyword);
-    counts = data.positive.map(item => item.count);
-    colors = Array(data.positive.length).fill('#1B92FF');
+    positiveLabels = data.positive.map(item => item.keyword);
+    positiveCounts = data.positive.map(item => item.count);
   } else if (type === 'NEGATIVE') {
-    labels = data.negative.map(item => item.keyword);
-    counts = data.negative.map(item => item.count);
-    colors = Array(data.negative.length).fill('#FF4D4D');
+    negativeLabels = data.negative.map(item => item.keyword);
+    negativeCounts = data.negative.map(item => item.count);
   }
 
-  const chartData = {
-    labels,
+  const positiveChartData = {
+    labels: positiveLabels,
     datasets: [
       {
-        label: 'Keyword Frequency',
-        data: counts,
-        backgroundColor: colors,
+        data: positiveCounts,
+        backgroundColor: '#1B92FF',
+        borderRadius: 8,
+      },
+    ],
+  };
+
+  const negativeChartData = {
+    labels: negativeLabels,
+    datasets: [
+      {
+        data: negativeCounts,
+        backgroundColor: '#FF4D4D',
         borderRadius: 8,
       },
     ],
@@ -63,12 +81,6 @@ export default function ReviewAnalysisChart({ type, data }: ReviewAnalysisChartP
       },
       title: {
         display: true,
-        text:
-          type === 'ALL'
-            ? 'Top 5 Positive & Negative Keywords'
-            : type === 'POSITIVE'
-              ? 'Top Positive Keywords'
-              : 'Top Negative Keywords',
       },
     },
     scales: {
@@ -79,8 +91,18 @@ export default function ReviewAnalysisChart({ type, data }: ReviewAnalysisChartP
   };
 
   return (
-    <div className="w-full mx-auto p-4 bg-white shadow-lg rounded-lg">
-      <Bar data={chartData} options={options} />
+    <div className="w-[100rem] mx-auto mt-[3rem] bg-white shadow-lg rounded-lg">
+      <p className="text-lg font-semibold text-blue-600 mb-2">TOP 5 긍정 & 부정 키워드</p>
+      {type !== 'NEGATIVE' && (
+        <div>
+          <Bar data={positiveChartData} options={options} />
+        </div>
+      )}
+      {type !== 'POSITIVE' && (
+        <div>
+          <Bar data={negativeChartData} options={options} />
+        </div>
+      )}
     </div>
   );
 }
