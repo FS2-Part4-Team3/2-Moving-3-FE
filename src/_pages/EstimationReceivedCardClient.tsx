155bc95bba575139319161f028aa8434f64061c8
hyobiin9 <moone1234@kakao.com>
2025-02-23 21:08:31 +0900

    feat:#401-initial structure

---
+++ src/_pages/EstimationReceivedCardClient.tsx
@@ -0,0 +1,9 @@
+import EstimateReceivedCard from '@/components/cards/EstimateReceivedCard';
+
+export default function EstimationReceivedCardClient() {
+  return (
+    <div>
+      <EstimateReceivedCard data={item} serviceType={data.serviceType} isConfirmed={false} />
+    </div>
+  );
+}
